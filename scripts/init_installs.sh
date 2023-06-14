cc15ee1047425b2953d67acf1653bd6be8b5ab23
chriswsuarez <chriswsuarez@utexas.edu>
2023-06-14 14:58:10 -0500

    Added more functionality to the initialize script

--- scripts/init_installs.sh
+++ scripts/init_installs.sh
@@ -14,11 +14,14 @@ else
     echo "Unsupported Ubuntu version"
     exit 1
 fi
-    
 
+# Add newline to bashrc before we make changes
+echo -e "\n" >> ~/.bashrc
 
 # Git
-sudo apt install git
+sudo apt install -y git
+git config --global user.name "chriswsuarez"
+git config --global user.email "chriswsuarez@utexas.edu"
 
 # Visual Studio code
 sudo snap install --classic code
@@ -27,25 +30,33 @@ sudo snap install --classic code
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install sublime-text
-
+sudo apt install -y sublime-text
 
 # catkin_tools
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 
+# pip
+if [[ $DISTRIB_RELEASE = "18.04" ]]; then
+    sudo apt install -y python-pip
+elif [[ $DISTRIB_RELEASE = "20.04" ]]; then
+    sudo apt install -y python3-pip
+else
+    echo "pip not installed"
+fi
+
 # Install ROS
-sudo apt install curl # if you haven't already installed curl
+sudo apt install -y curl # if you haven't already installed curl
 if [[ $DISTRIB_RELEASE = "18.04" ]]; then
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update
-    sudo apt install ros-melodic-desktop-full
+    sudo apt install -y ros-melodic-desktop-full
     echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 elif [[ $DISTRIB_RELEASE = "20.04" ]]; then
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update
-    sudo apt install ros-noetic-desktop-full
+    sudo apt install -y ros-noetic-desktop-full
     echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 else
     echo "ROS not installed"
