ab6454e926abc9d7485438463cae15dddd09deb3
chriswsuarez <chriswsuarez@utexas.edu>
2023-07-12 13:04:44 -0500

    Sublime install uses snap now

--- scripts/init_installs.sh
+++ scripts/init_installs.sh
@@ -27,10 +27,7 @@ git config --global user.email "chriswsuarez@utexas.edu"
 sudo snap install --classic code
 
 # Sublime 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install -y sublime-text
+sudo snap install sublime-text --classic
 
 # catkin_tools
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
